8de67928f24b8039462f3e97d934b0e243e2129b
Dávid Kolumber <kolumber@stud.uniza.sk>
2025-06-28 19:07:43 +0200

    Create profile view and javascript

--- App/Views/User/profile.view.php
+++ App/Views/User/profile.view.php
@@ -1,3 +1,15 @@
+<?php
+use App\Core\LinkGenerator;
+
+/** @var LinkGenerator $link */
+?>
+
 <div class="general-container">
-    Hello, User
+    <h2>Profil</h2>
+    <div
+        id="profile-component"
+        data-post="<?= $link->url("user.profile") ?>"
+        data-get="<?= $link->url("user.getUser") ?>"></div>
+
+    <script type="module" src="/public/js/profile.js"></script>
 </div>
